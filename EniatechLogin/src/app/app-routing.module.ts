bb083882dcac4ab3281433e82353f2f198f482b2
Fernanda Molina Aguilar <fer.molina070917@gmail.com>
2025-03-18 22:58:20 -0600

    creacion home

--- EniatechLogin/src/app/app-routing.module.ts
+++ EniatechLogin/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Importar los componentes de las páginas
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent}
   // Página 404
 ];
 
--- EniatechLogin/src/app/app.module.ts
+++ EniatechLogin/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule
